ca03505e85a172f39ed24ce1f0cb4451b90b3f9d
dzikimlecz <j.pul312@gmail.com>
2021-05-23 14:15:36 +0200

    fix this-2: committed changed implementation without the interface

--- src/main/java/me/dzikimlecz/chessapi/game/moveanalysing/IDrawAnalyser.java
+++ src/main/java/me/dzikimlecz/chessapi/game/moveanalysing/IDrawAnalyser.java
@@ -5,8 +5,10 @@ import me.dzikimlecz.chessapi.game.IMoveProcessor;
 import me.dzikimlecz.chessapi.game.moveparsing.IMoveValidator;
 import me.dzikimlecz.chessapi.game.movestoring.MoveDatabase;
 
+import java.util.Optional;
+
 public interface IDrawAnalyser extends IMoveProcessor {
-	DrawReason lookForDraw();
+	Optional<DrawReason> lookForDraw();
 
 	default void setMoveDatabase(MoveDatabase moveDatabase) {}
 
